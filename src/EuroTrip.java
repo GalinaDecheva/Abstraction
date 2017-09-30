f284bdee449fd152e80626b28222b27427a21068
Galina Decheva <de_galina@abv.bg>
2017-10-01 00:07:05 +0300

    euro trip

---
+++ src/EuroTrip.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class EuroTrip {
+public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//    double quantity = Double.parseDouble(input.nextLine());
+//    double pricePerKio = 1.20;
+//    BigDecimal priceInLeva = new BigDecimal(pricePerKio*quantity);
+//
+//    BigDecimal exchangeRate = new BigDecimal("4200");
+//    BigDecimal marksNeeded = exchangeRate.multiply(priceInLeva);
+//    System.out.printf("%.3f marks",priceInLeva);
+//
+//test();
+//}
+//    private static void test() {
+//        Scanner input = new Scanner(System.in);
+//        String firstName = input.nextLine();
+//        String secondName = input.nextLine();
+//        if (firstName.isEmpty()) {
+//            System.out.println("*****");
+//        }
+//        if (secondName.isEmpty()) {
+//            System.out.println("*****");
+//        }
+//        System.out.printf("Hello %s %s !", firstName, secondName);
+
+//    int primitive = 3;
+//    Integer revert = new Integer(primitive);
+
+//    Integer a = new Integer("3");
+//    Integer revert = a.parseInt("7");
+//
+//    System.out.println( a);
+//    System.out.println(revert);
+//    double jkh = 4.675675;
+//    float fdfd = (float)jkh;
+//    double aa = fdfd;
+
+//   String subject = input.nextLine();
+//   String compareString = input.nextLine();
+//   if (subject.equals(compareString) ){
+//       System.out.println(  );
+//   }
+//    String day = input.nextLine();
+//    switch (day){
+//        case "Monday":
+//            System.out.println("Monday");break;
+//        case "Friday":
+//            System.out.println( "Friday");break;
+//        default:
+//            System.out.println("Wrong day!");
+//    }
+
+
+    }
+}
+
