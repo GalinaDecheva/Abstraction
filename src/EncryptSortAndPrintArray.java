bd4ee2a7b900332b4a3ac894dd865a60f54da1f0
Galina Decheva <de_galina@abv.bg>
2017-10-01 00:28:45 +0300

    add vowel

--- src/EncryptSortAndPrintArray.java
+++ src/EncryptSortAndPrintArray.java
@@ -21,12 +21,11 @@ public class EncryptSortAndPrintArray {
 
     public static int[] encryptNames(String names[]) {
         List<Character> vowels = new ArrayList<>();
-        Collections.addAll(vowels, 'a', 'e', 'i', 'o', 'u');
+        Collections.addAll(vowels, 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
 
         int[] result = new int[names.length];
         for (int i = 0; i < names.length; i++) {
             int sum = 0;
-            //System.out.println(names[i]);
 
             for (int j = 0; j < names[i].length(); j++) {
                 if (vowels.contains(names[i].charAt(j))) {
