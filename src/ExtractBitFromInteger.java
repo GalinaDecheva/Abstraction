0d6bf6bf1c78c3764fbebe3d682fd7c56597a855
Galina Decheva <de_galina@abv.bg>
2017-10-01 00:16:25 +0300

    remove empty lines and change repo name

--- src/ExtractBitFromInteger.java
+++ src/ExtractBitFromInteger.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class ExtractBitFromInteger {
 public static void main(String[] arg){
     Scanner input = new Scanner(System.in);
-
     int n = input.nextInt();
     int p = input.nextInt();
     int mask = n >> p;
