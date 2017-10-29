2f5644d9e9927a6d6cd216544de823ce68034ccb
Galina Decheva <de_galina@abv.bg>
2017-10-29 10:02:05 +0200

    Sum Matrix Element

---
+++ src/MainPerson.java
@@ -0,0 +1,11 @@
+public class MainPerson {
+
+    public static void main(String[] args) {
+    Person Ivan = new Person("Ivan");
+    Person Kamelia = new Person("Kamelia",16);
+
+        System.out.println(Ivan);
+    }
+
+
+}
